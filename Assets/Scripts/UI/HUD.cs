186a5eab8429900e5f68b97f346ea340b0ae122a
Patrykowy <Patrykowy@DESKTOP-SCTLCSD>
2026-07-04 23:32:49 +0200

    Commit #2 -- wave numb

--- Assets/Scripts/UI/HUD.cs
+++ Assets/Scripts/UI/HUD.cs
@@ -3,28 +3,48 @@ using TMPro;
 
 public class HUD : MonoBehaviour
 {
+    [Header("Texts")]
     [SerializeField] TextMeshProUGUI moneyText;
     [SerializeField] TextMeshProUGUI livesText;
+    [SerializeField] TextMeshProUGUI waveText;
+
+    [Header("Panels")]
     [SerializeField] GameObject gameOverPanel;
     [SerializeField] GameObject gameWonPanel;
+
+    [Header("References")]
+    [SerializeField] WaveSpawner waveSpawner;
 
     void OnEnable()
     {
         var game = GameManager.Instance;
+
         game.MoneyChanged += UpdateMoney;
         game.LivesChanged += UpdateLives;
         game.GameEnded += OnGameEnded;
 
         UpdateMoney(game.Money);
         UpdateLives(game.Lives);
+
+        if (waveSpawner != null)
+        {
+            waveSpawner.WaveChanged += UpdateWave;
+            UpdateWave(waveSpawner.CurrentWaveNumber, waveSpawner.TotalWaves);
+        }
     }
 
     void OnDisable()
     {
         var game = GameManager.Instance;
+
         game.MoneyChanged -= UpdateMoney;
         game.LivesChanged -= UpdateLives;
         game.GameEnded -= OnGameEnded;
+
+        if (waveSpawner != null)
+        {
+            waveSpawner.WaveChanged -= UpdateWave;
+        }
     }
 
     void UpdateMoney(int money)
@@ -35,6 +55,14 @@ public class HUD : MonoBehaviour
     void UpdateLives(int lives)
     {
         livesText.text = "HP: " + lives;
+    }
+
+    void UpdateWave(int currentWave, int totalWaves)
+    {
+        if (waveText == null)
+            return;
+
+        waveText.text = "WAVE: " + currentWave + " / " + totalWaves;
     }
 
     void OnGameEnded(bool isGameWon)
